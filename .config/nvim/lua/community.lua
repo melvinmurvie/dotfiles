92f0c3763a46be21fd2559ca696c1b171ec63e7f
Melvin Murvie <melvin.murvie@gmail.com>
2025-05-16 14:12:09 +0800

    chore(nvim): remove duplicate config
    Don't know what the differences with lazy if it is loaded first (in
    community.lua) and in plugins directory

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -73,37 +73,4 @@ return {
 
   -- Docker terminal https://github.com/AstroNvim/astrocommunity/blob/main/lua/astrocommunity/docker/lazydocker
   { import = "astrocommunity.docker.lazydocker" },
-
-  {
-    "lewis6991/hover.nvim",
-    lazy = true,
-    specs = {
-      {
-        "AstroNvim/astrocore",
-        opts = {
-          options = { opt = { mousemoveevent = true } },
-          mappings = {
-            n = {
-              -- Disable hover from hover plugin and uses default lsp from AstroNvim
-              -- mainly because hover plugin does not have scroll functionality (yet).
-              -- K = { function() require("hover").hover() end, desc = "Hover cursor" },
-              gh = { function() require("hover").hover_select() end, desc = "Hover selection" },
-              ["]h"] = { function() require("hover").hover_switch "next" end, desc = "Next hover source" },
-              ["[h"] = { function() require("hover").hover_switch "previous" end, desc = "Previous hover source" },
-              ["<MouseMove>"] = { function() require("hover").hover_mouse() end, desc = "Hover mouse" },
-            },
-          },
-        },
-      },
-      -- {
-      --   "AstroNvim/astrolsp",
-      --   opts = { mappings = { n = { K = false } } },
-      -- },
-    },
-    opts = {
-      init = function() require "hover.providers.lsp" end,
-      mouse_delay = 300,
-      preview_window = false,
-    },
-  },
 }
