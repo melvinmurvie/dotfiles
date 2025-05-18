fd001406d781826231e04177003a01362e62fcbe
Melvin Murvie <melvin.murvie@gmail.com>
2025-05-19 00:47:13 +0800

    feat(tmux): add initial tmux config leveraging oh-my-tmux

--- .config/zsh/custom/exports.zsh
+++ .config/zsh/custom/exports.zsh
@@ -6,3 +6,8 @@ export PATH=/usr/local/go/bin:$HOME/go/bin:$PATH
 
 # Snap weirdly get omitted sometimes from path
 export PATH=/snap/bin:$PATH
+
+# Override tpm installation path because of our taskfile script will remove it
+# since the default installation is in the same directory with this tmux conf
+export TMUX_PLUGIN_MANAGER_PATH=~/.tmux/plugins
+
