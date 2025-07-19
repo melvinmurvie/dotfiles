86fefb72619f2fd263e061434c8449d3868d6143
Melvin Murvie <melvin.murvie@gmail.com>
2025-07-19 22:41:30 +0800

    feat(arch): migrating to arch with hyprland

--- .config/nvim/lua/plugins/smear.lua
+++ .config/nvim/lua/plugins/smear.lua
@@ -1,4 +1,4 @@
--- if true then return {} end
+if true then return {} end
 
 return {
   -- https://github.com/sphamba/smear-cursor.nvim
--- .config/zsh/custom/plugins.zsh
+++ .config/zsh/custom/plugins.zsh
@@ -1,10 +1,12 @@
 # Syntax highlighting
 #   Installed with `sudo apt install zsh-syntax-highlighting`.
-source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+# source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 # sudo Auto suggestions
 #   Installed with `sudo apt install zsh-autosuggestions`.
-source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+# source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 
 # History substring search, this need to set after `zsh-syntax-highlighting` otherwise it will cause some highlighting bug (flickering)
 #   Installed with omz but sourced manually.
