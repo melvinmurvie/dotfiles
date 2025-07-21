b74ed70aa592f28b62cb7d61ca488b9694354e04
Melvin Murvie <melvin.murvie@gmail.com>
2025-07-21 23:10:07 +0800

    fix(nvim): fix yaml schema store not loaded
    The fix should be to set optional = false, but this include refactor of
    custom schema

--- .config/nvim/lua/plugins/yaml.lua
+++ .config/nvim/lua/plugins/yaml.lua
@@ -1,7 +1,7 @@
 return {
   "b0o/SchemaStore.nvim",
   lazy = true,
-  optional = true,
+  -- optional = true,
   dependencies = {
     {
       "AstroNvim/astrolsp",
@@ -12,21 +12,38 @@ return {
         config = {
           yamlls = {
             on_new_config = function(config)
-              local schemastore_schemas = require("schemastore").yaml.schemas {
-                -- Disable this since it conflicts with my custom taksfile path
-                ignore = { "Ansible Tasks File" },
-              }
-              local custom_schemas = {
-                ["https://taskfile.dev/schema.json"] = {
-                  "Taskfile.yaml",
-                  "Taskfile.dist.yaml",
-                  "Taskfile.yml",
-                  "Taskfile.dist.yml",
-                  "**/scripts/tasks/*.yml",
-                },
-              }
-              config.settings.yaml.schemas =
-                vim.tbl_deep_extend("force", config.settings.yaml.schemas or {}, schemastore_schemas, custom_schemas)
+              config.settings.yaml.schemas = vim.tbl_deep_extend(
+                "force",
+                config.settings.yaml.schemas or {},
+                require("schemastore").yaml.schemas {
+                  -- Disable this since it conflicts with my custom taksfile path
+                  ignore = { "Ansible Tasks File" },
+                  extra = {
+                    {
+                      name = "Custom Taskfile",
+                      url = "https://taskfile.dev/schema.json",
+                      fileMatch = {
+                        "Taskfile.yaml",
+                        "Taskfile.dist.yaml",
+                        "Taskfile.yml",
+                        "Taskfile.dist.yml",
+                        "**/scripts/tasks/*.yml",
+                      },
+                    },
+                  },
+                }
+              )
+              -- local custom_schemas = {
+              --   ["https://taskfile.dev/schema.json"] = {
+              --     "Taskfile.yaml",
+              --     "Taskfile.dist.yaml",
+              --     "Taskfile.yml",
+              --     "Taskfile.dist.yml",
+              --     "**/scripts/tasks/*.yml",
+              --   },
+              -- }
+              -- config.settings.yaml.schemas =
+              --   vim.tbl_deep_extend("force", config.settings.yaml.schemas or {}, schemastore_schemas, custom_schemas)
             end,
             settings = {
               yaml = {
