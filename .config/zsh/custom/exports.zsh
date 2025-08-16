46a8c473fce4499a44d40fcb03beceede81ab759
Melvin Murvie <melvin.murvie@gmail.com>
2025-08-16 17:44:02 +0800

    feat(bin): add initial bin path to data home directory and add to zsh PATH

--- .config/zsh/custom/exports.zsh
+++ .config/zsh/custom/exports.zsh
@@ -4,8 +4,8 @@ export PATH=$HOME/bin:$HOME/.local/bin:/usr/local/bin:$PATH
 # Golang
 export PATH=/usr/local/go/bin:$HOME/go/bin:$PATH
 
-# Snap weirdly get omitted sometimes from path
-export PATH=/snap/bin:$PATH
+# Desktop
+export PATH=${XDG_DATA_HOME:-$HOME/.local/share}/fnf/bin:$PATH
 
 # Override tpm installation path because of our taskfile script will remove it
 # since the default installation is in the same directory with this tmux conf
