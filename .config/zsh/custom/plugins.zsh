4a5aaa54ee05b6cdacc40b515423b191aba1fa6b
Melvin Murvie <melvin.murvie@gmail.com>
2025-08-16 17:42:56 +0800

    chore(zsh): remove unused bindings

--- .config/zsh/custom/plugins.zsh
+++ .config/zsh/custom/plugins.zsh
@@ -15,19 +15,19 @@ source $ZSH/plugins/history-substring-search/history-substring-search.plugin.zsh
 # Vi mode
 #   Installed with plugins defined in `.zshrc`.
 VI_MODE_SET_CURSOR=true
-VI_MODE_RESET_PROMPT_ON_MODE_CHANGE=true
+# VI_MODE_RESET_PROMPT_ON_MODE_CHANGE=true
 
 # History substring search
 #   Installed with plugins in .zshrc
-bindkey '^[[A' history-substring-search-up
-bindkey '^[[B' history-substring-search-down
-
-bindkey -M vicmd 'k' history-substring-search-up
-bindkey -M vicmd 'j' history-substring-search-down
-bindkey -M vicmd '^P' history-substring-search-up
-bindkey -M vicmd '^N' history-substring-search-down
-bindkey -M viins '^P' history-substring-search-up
-bindkey -M viins '^N' history-substring-search-down
-
-bindkey -M emacs '^P' history-substring-search-up
-bindkey -M emacs '^N' history-substring-search-down
+# bindkey '^[[A' history-substring-search-up
+# bindkey '^[[B' history-substring-search-down
+#
+# bindkey -M vicmd 'k' history-substring-search-up
+# bindkey -M vicmd 'j' history-substring-search-down
+# bindkey -M vicmd '^P' history-substring-search-up
+# bindkey -M vicmd '^N' history-substring-search-down
+# bindkey -M viins '^P' history-substring-search-up
+# bindkey -M viins '^N' history-substring-search-down
+#
+# bindkey -M emacs '^P' history-substring-search-up
+# bindkey -M emacs '^N' history-substring-search-down
