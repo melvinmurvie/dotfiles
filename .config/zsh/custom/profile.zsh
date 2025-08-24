4c6285fd16ee69ebcd8c66a5c31c833f1c4c8dce
Melvin Murvie <melvin.murvie@gmail.com>
2025-08-24 16:00:59 +0800

    chore(zsh): move exports settings to profile file

--- .config/zsh/custom/profile.zsh
+++ .config/zsh/custom/profile.zsh
@@ -1,5 +1,21 @@
+# If you come from bash you might have to change your $PATH.
+export PATH=$HOME/bin:$HOME/.local/bin:/usr/local/bin:$PATH
+
+# Golang
+export PATH=/usr/local/go/bin:$HOME/go/bin:$PATH
+
+# Desktop
+export PATH=${XDG_DATA_HOME:-$HOME/.local/share}/fnf/bin:$PATH
+
+# Override tpm installation path because of our taskfile script will remove it
+# since the default installation is in the same directory with this tmux conf
+export TMUX_PLUGIN_MANAGER_PATH=~/.tmux/plugins
+
 # Set default editor to nvim
 export EDITOR="nvim"
+
+# Set default terminal emulator, some applications may use this
+export TERMINAL="ghostty"
 
 # Activate 'oh-my-posh' if Installed
 if (( ${+commands[oh-my-posh]} )); then
