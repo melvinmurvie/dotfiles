8d4e552c3e297bb02e44c6c03efd7dce2597db31
Melvin Murvie <melvin.murvie@gmail.com>
2025-10-11 23:12:55 +0800

    fix(init): fix unquoted variables and shebang definition

--- init.sh
+++ init.sh
@@ -1,9 +1,9 @@
-#/usr/bin/env bash
+#!/usr/bin/env bash
 
 set -eu
 
 filename=task_linux_amd64.tar.gz
 tmpdir=$(mktemp --directory)
 
-curl --silent --show-error --location --output-dir ${tmpdir} --remote-name https://github.com/go-task/task/releases/download/v3.43.3/${filename}
-tar --directory ${HOME}/.local/bin -xzf ${tmpdir}/${filename} task
+curl --silent --show-error --location --output-dir "${tmpdir}" --remote-name "https://github.com/go-task/task/releases/download/v3.43.3/${filename}"
+tar --directory "${HOME}/.local/bin" -xzf "${tmpdir}/${filename}" task
