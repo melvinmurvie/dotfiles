aa92f132cd58929e878141fea27e700ab0ba97d2
Melvin Murvie <melvin.murvie@gmail.com>
2026-01-06 21:34:23 +0800

    feat(zsh): update profile for plugins ensure its path exists for operating systems
    I have 2 systems: Arch and Debian (and Ubuntu)
    Each system installs the plugin on different paths

--- .config/zsh/custom/plugins.zsh
+++ .config/zsh/custom/plugins.zsh
@@ -1,12 +1,20 @@
 # Syntax highlighting
-#   Installed with `sudo apt install zsh-syntax-highlighting`.
-# source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+# Debian
+if [[ -f /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]]; then
+  source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+# Arch
+elif [[ -f /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]]; then
+  source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+fi
 
-# sudo Auto suggestions
-#   Installed with `sudo apt install zsh-autosuggestions`.
-# source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+# Auto suggestions
+# Debian
+if [[ -f /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh ]]; then
+  source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+# Arch
+elif [[ -f /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh ]]; then
+  source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+fi
 
 # History substring search, this need to set after `zsh-syntax-highlighting` otherwise it will cause some highlighting bug (flickering)
 #   Installed with omz but sourced manually.
