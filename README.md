87b82c16991aafcc92323d84a2f547da0fb30bf8
Melvin Murvie <melvin.murvie@gmail.com>
2026-01-20 22:08:32 +0800

    feat: update docs and init script

--- README.md
+++ README.md
@@ -4,4 +4,8 @@ Configurations with Arch Linux (Hyprland)
 
 Most configuration scripts are written in [Taskfile]("https://github.com/go-task/task") scripts. Install it by running [init.sh](./init.sh) first.
 
-Finally just run `task`.
+## Steps
+
+1. Run `init.sh`.
+2. Run `mise use --global task`.
+3. Finally just run `task`.
--- init.sh
+++ init.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-set -eu
+set -euo pipefail
 
-filename=task_linux_amd64.tar.gz
-tmpdir=$(mktemp --directory)
+install_dir="${HOME}/.local/bin"
 
-curl --silent --show-error --location --output-dir "${tmpdir}" --remote-name "https://github.com/go-task/task/releases/download/v3.43.3/${filename}"
-tar --directory "${HOME}/.local/bin" -xzf "${tmpdir}/${filename}" task
+[ -d "${install_dir}" ] || mkdir -p "${install_dir}"
+[ -f "${install_dir}/mise" ] && exit 0
+
+export MISE_INSTALL_PATH="${install_dir}/mise"
+curl https://mise.run | sh
