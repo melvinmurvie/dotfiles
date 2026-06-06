fa78a68bfa4916f201ddb605ec78f3a4c0d9a03e
Melvin Murvie <melvin.murvie@gmail.com>
2026-06-06 14:54:14 +0800

    fix(nvim): fix yaml schema store LSP setting for Nvim > 0.12 version

--- .config/nvim/lua/plugins/yaml.lua
+++ .config/nvim/lua/plugins/yaml.lua
@@ -1,55 +1,14 @@
+---@type LazySpec
 return {
-  "b0o/SchemaStore.nvim",
-  lazy = true,
-  -- optional = true,
-  dependencies = {
-    {
-      "AstroNvim/astrolsp",
-      optional = true,
-      ---@type AstroLSPOpts
-      opts = {
-        ---@diagnostic disable: missing-fields
-        config = {
-          yamlls = {
-            on_new_config = function(config)
-              config.settings.yaml.schemas = vim.tbl_deep_extend(
-                "force",
-                config.settings.yaml.schemas or {},
-                require("schemastore").yaml.schemas {
-                  -- Disable this since it conflicts with my custom taksfile path
-                  ignore = { "Ansible Tasks File" },
-                  extra = {
-                    {
-                      name = "Custom Taskfile",
-                      url = "https://taskfile.dev/schema.json",
-                      fileMatch = {
-                        "Taskfile.yaml",
-                        "Taskfile.dist.yaml",
-                        "Taskfile.yml",
-                        "Taskfile.dist.yml",
-                        "**/scripts/tasks/*.yml",
-                      },
-                    },
-                  },
-                }
-              )
-              -- local custom_schemas = {
-              --   ["https://taskfile.dev/schema.json"] = {
-              --     "Taskfile.yaml",
-              --     "Taskfile.dist.yaml",
-              --     "Taskfile.yml",
-              --     "Taskfile.dist.yml",
-              --     "**/scripts/tasks/*.yml",
-              --   },
-              -- }
-              -- config.settings.yaml.schemas =
-              --   vim.tbl_deep_extend("force", config.settings.yaml.schemas or {}, schemastore_schemas, custom_schemas)
-            end,
-            settings = {
-              yaml = {
-                schemaStore = { enable = false, url = "" },
-              },
-            },
+  "AstroNvim/astrolsp",
+  dependencies = { "b0o/schemastore.nvim" },
+  opts = {
+    config = {
+      yamlls = {
+        settings = {
+          yaml = {
+            schemaStore = { enable = false, url = "" },
+            schemas = require("schemastore").yaml.schemas(),
           },
         },
       },
