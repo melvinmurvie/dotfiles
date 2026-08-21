319ec0f0bb93547577390d7cb7c98395820c53cc
Melvin Murvie <melvin.murvie@gmail.com>
2026-08-21 22:47:10 +0800

    chore(hypr): update top command to run command directly instead of wrapping it in sh session

--- .config/hypr/global.lua
+++ .config/hypr/global.lua
@@ -15,11 +15,10 @@ M.lock = "uwsm app -- hyprlock"
 M.webapp = M.browser .. " --new-window --app"
 M.nc = "swaync-client --toggle-panel"
 M.fnfMenu = "uwsm app -- ~/.local/share/fnf/bin/fnf-menu"
-M.top = M.terminal .. " sh -c 'btop'"
+M.top = M.terminal .. " -e btop"
 
 M.hyprshot = "uwsm app -- hyprshot"
 M.sattyEditStdin = "uwsm app -- satty --actions-on-right-click save-to-clipboard --filename -"
 M.ocr4 = "uwsm app -- OCR4Linux.sh -l --lang all"
 
 return M
-
