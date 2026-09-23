046d546acac128e658fbc4177e72d00c06270637
Melvin Murvie <melvin.murvie@gmail.com>
2026-09-24 00:23:40 +0800

    feat(themes): add catppuccin latte theme

---
+++ themes/catppuccin-latte/hyprland.lua
@@ -0,0 +1,30 @@
+-- Catppuccin Mocha theme: Hyprland overrides
+
+hl.config({
+  general = {
+    col = {
+      active_border = { colors = { "rgb(1e66f5)", "rgb(8839ef)" }, angle = 45 },
+      inactive_border = "rgb(eff1f5)",
+    },
+  },
+
+  decoration = {
+    shadow = {
+      color = "rgb(dce0e8)",
+    },
+  },
+
+  group = {
+    col = {
+      border_active = { colors = { "rgb(1e66f5)", "rgb(8839ef)" }, angle = 45 },
+      border_inactive = "rgb(eff1f5)",
+    },
+
+    groupbar = {
+      col = {
+        active = "rgb(7287fd)",
+      },
+      text_color = "rgb(4c4f69)",
+    },
+  },
+})
